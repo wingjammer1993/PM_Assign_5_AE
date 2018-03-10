924b5c1dc8aa8c6ef2c18b75137140a414177dcf
Amruta <hail.radical@gmail.com>
2018-03-09 20:28:35 -0700

    assignment

--- Part_I.py
+++ Part_I.py
@@ -1,25 +1,23 @@
 from scipy.stats import gamma
-from scipy.stats import norm
 import math
 import numpy as np
 
 
 # I = 1, M = 2, U = 3, S = 4
-def give_likelihood_weighting(num_samples):
+def give_likelihood_weighting(num_samples, salary_weight):
     # Fix evidence variables
     samples = {}
     for i in range(0, num_samples):
         intell = 100 + np.random.randn()*15
         major = np.random.rand() < 1/(1 + math.exp(-(intell - 110)/5))
         uni = np.random.rand() < 1/(1 + math.exp(-(intell - 110)/5))
-        salary = np.random.gamma(0.1*intell + major + 3*uni, 5)
-        weight = gamma.pdf(salary, 0.1*intell + major + 3*uni, 5)
+        weight = gamma.pdf(salary_weight, 0.1*intell + major + 3*uni, 5)
         samples[(intell, major, uni, weight)] = weight
     return samples
 
 
 if __name__ == "__main__":
     num = 10000
-    dict_sample = give_likelihood_weighting(num)
+    dict_sample = give_likelihood_weighting(num, 120)
     print(dict_sample)
 
