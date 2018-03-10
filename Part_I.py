b30d633ced3023e61febcdb08ce1dab05f14d862
Amruta <hail.radical@gmail.com>
2018-03-09 20:44:35 -0700

    assignment

--- Part_I.py
+++ Part_I.py
@@ -16,8 +16,34 @@ def give_likelihood_weighting(num_samples, salary_weight):
     return samples
 
 
+def compute_joint_posterior(likelihood_samples):
+    joint_post = {('cs', 'met'): 0, ('cs', 'uc'): 0, ('biz', 'met'): 0, ('biz', 'uc'): 0}
+    for sample in likelihood_samples:
+        if sample[1]:
+            if sample[2]:
+                joint_post[('cs', 'uc')] += likelihood_samples[sample]
+            else:
+                joint_post[('cs', 'met')] += likelihood_samples[sample]
+        else:
+            if sample[2]:
+                joint_post[('biz', 'uc')] += likelihood_samples[sample]
+            else:
+                joint_post[('biz', 'met')] += likelihood_samples[sample]
+    joint_post = {k: v/sum(joint_post.values()) for k, v in joint_post.items()}
+    return joint_post
+
+
 if __name__ == "__main__":
     num = 10000
-    dict_sample = give_likelihood_weighting(num, 120)
-    print(dict_sample)
+    dict_sample_1 = give_likelihood_weighting(num, 120)
+    posterior_1 = compute_joint_posterior(dict_sample_1)
+    print(posterior_1)
+
+    dict_sample_2 = give_likelihood_weighting(num, 60)
+    posterior_2 = compute_joint_posterior(dict_sample_2)
+    print(posterior_2)
+
+    dict_sample_3 = give_likelihood_weighting(num, 20)
+    posterior_3 = compute_joint_posterior(dict_sample_3)
+    print(posterior_3)
 
