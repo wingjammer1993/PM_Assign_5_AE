8af7420225825d8f4b65c720fb8fcb364fe575e4
wingjammer <hail.radical@gmail.com>
2018-03-21 20:31:47 -0600

    commit

--- part_1.py
+++ part_1.py
@@ -13,13 +13,13 @@ with pm.Model() as dna_model:
     g1 = pm.Bernoulli('g1', .5)
 
     # Create a variable g2 with Bernoulli distribution.
-    # If g1 = 0, g2 = 1 with probability 0.1 and g2 = 0 with probability 0.9
-    # If g1 = 1, g2 = 1 with probability 0.9 and g2 = 0 with probability 0.1
+    # If condition is TRUE, g1 = 0, g2 = 1 with probability 0.1 and g2 = 0 with probability 0.9
+    # If condition is FALSE, g1 = 1, g2 = 1 with probability 0.9 and g2 = 0 with probability 0.1
     g2 = pm.Bernoulli('g2', pm.math.switch(g1 < 1, 0.1, 0.9))
 
     # Create a variable g3 with Bernoulli distribution.
-    # If g1 = 0, g3 = 1 with probability 0.1 and g3 = 0 with probability 0.9
-    # If g1 = 1, g3 = 1 with probability 0.9 and g3 = 0 with probability 0.1
+    # If condition is TRUE, g1 = 0, g3 = 1 with probability 0.1 and g3 = 0 with probability 0.9
+    # If condition is FALSE, g1 = 1, g3 = 1 with probability 0.9 and g3 = 0 with probability 0.1
     g3 = pm.Bernoulli('g3', pm.math.switch(g1 < 1, 0.1, 0.9))
 
     # Create temporary variables var_1, var_2 and var_3 which depend on g1, g2, g3 respectively.
@@ -58,19 +58,34 @@ with dna_model:
     print(pm.summary(trace))
     plt.show()
 
+    # Part I
     trace_g1 = trace['g1'][:].tolist()
-
     # Find the conditional probability P(G1=1|X2=50)
-    # P(G1=1|X2=50) =  P(G1=1,X2=50)/ [P(G1=0|X2=50) +  P(G1=1|X2=50)]
+    # P(G1=1|X2=50) =  P(G1=1,X2=50)/  P(X2=50)
+    # P(G1=1|X2=50) =  P(G1=1,X2=50)/ [P(G1=0,X2=50) +  P(G1=1,X2=50)]
     # P(G1=1|X2=50) = #samples with G1 = 1 / #total samples
-    count = 0
+    count_1 = 0
     for i in trace_g1:
         if i == 1:
-            count += 1
+            count_1 += 1
 
-    probability_conditional = count/float(len(trace_g1))
+    probability_conditional_1 = count_1/float(len(trace_g1))
 
-    print(probability_conditional)
+    # Part II
+    trace_x1 = trace['x3'][:].tolist()
+    # Find the conditional probability P(X3=50|X2=50)
+    # P(X3=50|X2=50) =  P(X3=50,X2=50)/ P(X2=50)
+    # P(X3=50,X2=50) is approximated using P(49.5<X3<50.5,X2=50)
+    # P(X3=50|X2=50) = #samples with 49.5<X3<50.5 / #total samples
+    count_2 = 0
+    for i in trace_x1:
+        if 49.5 < i < 50.5:
+            count_2 += 1
+
+    probability_conditional_2 = count_2/float(len(trace_x1))
+
+    print(probability_conditional_1)
+    print(probability_conditional_2)
 
 
 
